a541785f178f62e21170f2a3ee7ed89bc5e27b08
Mati <mati@pjwstk.edu.pl>
2017-05-30 19:17:04 +0200

    tried fixing gain not showing up

--- USG_tablet_UI/Pages/Urzadzenia.xaml.cs
+++ USG_tablet_UI/Pages/Urzadzenia.xaml.cs
@@ -37,18 +37,18 @@ namespace USG_tablet_UI
             //GlobalSettings.paramListener = new TCPlistener(12000);
             //GlobalSettings.paramListener.connect();
             GlobalSettings.reader = new StreamReader(GlobalSettings.clientSocket.GetStream());
-            if (GlobalSettings.gainRefreshTimer == null)
+            /*if (GlobalSettings.gainRefreshTimer == null)
             {            
                 GlobalSettings.gainRefreshTimer = new DispatcherTimer();
                 GlobalSettings.gainRefreshTimer.Tick += new EventHandler(refreshGain);
                 GlobalSettings.gainRefreshTimer.Interval = new TimeSpan(0, 0, 0, 0, 500);
 
-            }
+            }*/
             //GlobalSettings.gainRefreshTimer.Start(); 
             refreshDepth();
-            Thread.Sleep(10);
+            Thread.Sleep(20);
             refreshTx();
-            Thread.Sleep(10);
+            Thread.Sleep(20);
             refreshGain();
         }
 
@@ -69,7 +69,7 @@ namespace USG_tablet_UI
             refreshGain();
         }
 
-        private void refreshGain(object sender, EventArgs e)
+        /*private void refreshGain(object sender, EventArgs e)
         {
             if (GlobalSettings.gainRequestCompleted == true)
             {
@@ -87,7 +87,7 @@ namespace USG_tablet_UI
                     catch (Exception ex) { GlobalSettings.gainRequestCompleted = true; };
                 }).Start();
             }
-        }
+        }*/
 
         private void refreshGain()
         {
